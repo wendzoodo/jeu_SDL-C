d467fd20c97a2cb40e5aed05536729d869d7d2b9
yasminemanita <47643519+yasminemanita@users.noreply.github.com>
2019-04-04 21:25:20 +0100

    Add files via upload

---
+++ map.h.h
@@ -0,0 +1,14 @@
+#ifndef MAP_H_INCLUDED
+#define MAP_H_INCLUDED
+
+
+typedef struct Map
+  {
+      SDL_Surface  *fond,*f;
+      
+      SDL_Rect post,post1;
+ 
+
+}Map;
+
+#endif //INPUT_H_INCLUDED
